737e8aabd4f5b7d97afc0c6240106706a154c365
Chiga754 <a.chigarev96@gmail.com>
2022-02-07 16:04:26 +0500

    Вертска секции header

--- src/js/plugins/materialize.js
+++ src/js/plugins/materialize.js
@@ -1,2 +1,10 @@
 import 'materialize-css/dist/css/materialize.min.css';
 import 'materialize-css/dist/js/materialize.min.js';
+
+// Init select
+const select = document.querySelectorAll('select');
+M.FormSelect.init(select);
+
+export function getSelectInstance(element) {
+    return M.FormSelect.getInstance(element);
+}
