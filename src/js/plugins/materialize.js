bde6926acc06255a48a02306febfb65309401ef2
Chiga754 <a.chigarev96@gmail.com>
2022-02-07 20:53:02 +0500

    Create UI

--- src/js/plugins/materialize.js
+++ src/js/plugins/materialize.js
@@ -5,6 +5,28 @@ import 'materialize-css/dist/js/materialize.min.js';
 const select = document.querySelectorAll('select');
 M.FormSelect.init(select);
 
+const autocomplete = document.querySelectorAll('.autocomplete');
+M.Autocomplete.init(autocomplete, {
+    data: {
+      "Apple": null,
+      "Microsoft": null,
+      "Google": 'https://placehold.it/250x250'
+    },
+  });
+
+const datepicker = document.querySelectorAll('.datepicker');
+M.Datepicker.init(datepicker, {
+    showClearBtn : true,
+});
+
 export function getSelectInstance(element) {
     return M.FormSelect.getInstance(element);
+}
+
+export function getAutocompleteInstance(element) {
+    return M.Autocomplete.getInstance(element);
+}
+
+export function getDatepickerInstance(element) {
+    return M.Datepicker.getInstance(element);
 }
